4a3459b9d2ae164f7e68be4f0cd8b726a3ae079e
kieron <kieronboz@gmail.com>
2024-02-08 18:15:16 +0000

    Update manifest and service worker paths

--- service-worker.js
+++ service-worker.js
@@ -3,27 +3,26 @@
  * https://github.com/TheNocoder/pwa-via-iframe
  */
 
-self.addEventListener('install', function(event) {
-    var offlineRequest = new Request('/ipwa/offline.html');
-    event.waitUntil(
-        fetch(offlineRequest).then(function(response) {
-            return caches.open('offline').then(function(cache) {
-                return cache.put(offlineRequest, response);
-            });
-        })
+self.addEventListener("install", function (event) {
+  var offlineRequest = new Request("offline.html");
+  event.waitUntil(
+    fetch(offlineRequest).then(function (response) {
+      return caches.open("offline").then(function (cache) {
+        return cache.put(offlineRequest, response);
+      });
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  var request = event.request;
+  if (request.method === "GET") {
+    event.respondWith(
+      fetch(request).catch(function (error) {
+        return caches.open("offline").then(function (cache) {
+          return cache.match("offline.html");
+        });
+      })
     );
+  }
 });
-
-self.addEventListener('fetch', function(event) {
-    var request = event.request;
-    if (request.method === 'GET') {
-        event.respondWith(
-            fetch(request).catch(function(error) {
-                return caches.open('offline').then(function(cache) {
-                    return cache.match('/ipwa/offline.html');
-                });
-            })
-        );
-    }
-});
-
